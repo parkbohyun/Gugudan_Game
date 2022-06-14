8c13c02d462e0674819a581840966a80a49a4774
Bo._.H <47629333+parkbohyun@users.noreply.github.com>
2022-06-15 08:28:12 +0900

    Add files via upload

---
+++ Gugudan_Game.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Gugudan_Game {
+	  public static void main(String[] args) throws IOException{
+		  int x, y;
+		  Random r= new Random();
+
+		  if(args.length==1){
+			  x= Integer.valueOf(args[0]).intValue(); 
+			  //랩퍼 클래스 intValue()는 Integer 객체로 생성된 내용 값을 int형으로 형변환
+		  }else{
+			  x= Math.abs(r.nextInt() % 9) + 1;
+		  }
+	
+		  y= Math.abs(r.nextInt() % 9) + 1;
+		  
+		  int num= x*y;
+	
+		  System.out.println();
+		  System.out.println("* 구구단 "+ x + "단에 대한 문제입니다");
+		  System.out.println();
+	
+	
+		  System.out.print(x +" * "+ y +" = ");
+		 
+		  BufferedReader in= new BufferedReader(new InputStreamReader(System.in));
+		  
+		  String user;
+		  user= in.readLine();
+	
+
+		  int inputNum = Integer.parseInt(user);
+		  //랩퍼클래스 parseInt()는 static으로 Integer 객체를 생성하지 않고, parameter로 int형으로 형변환
+	
+		  if(num==inputNum){
+			  System.out.println("맞았습니다!");
+		  }else{
+			  System.out.println("틀렸습니다. 정답은 "+ num +"입니다.");
+		  }
+	  }
+}
+
